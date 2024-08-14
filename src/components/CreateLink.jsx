0a4e7ac4296d6046005d477ff3b6101d2e970cdb
Ennypitan Adekoya <ennypitan@gmail.com>
2024-08-14 16:30:15 +0100

    changes

--- src/components/CreateLink.jsx
+++ src/components/CreateLink.jsx
@@ -1,5 +1,48 @@
+import { UrlState } from "@/context";
 import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import Error from "./Error";
+import { Card } from "./ui/card";
 
 export const CreateLink = () => {
-  return <div>CreateLink</div>;
+  const { user } = UrlState();
+  const navigate = useNavigate();
+  let [searchParams, setSearchParams] = useSearchParams();
+  const longLink = searchParams.get("createNew");
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger>
+          <Button>Create New Link</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle className="font-bold text-2xl">Create New</DialogTitle>
+          </DialogHeader>
+          <Input id="title" placeholder="Short Link's Title" />
+          <Error message={"Some error"} />
+          <Input id="title" placeholder="Enter Your URL" />
+          <Error message={"Some error"} />
+          <div className="flex items-center gap-2">
+            <Card className="p-2"> tinypath.io</Card> /
+            <Input id="title" placeholder="Custom Link(Optinal)" />
+          </div>
+          <Error message={"Some error"} />
+          <DialogFooter className="sm:justify-start">
+            <Button variant="destructive">Create</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,6 +10,7 @@ import { UrlState } from "@/context";
 import { getClicksForUrls } from "@/db/apiClicks";
 import { getUrls } from "@/db/apiUrls";
 import LinkCard from "@/components/LinkCard";
+import { CreateLink } from "@/components/CreateLink";
 
 const Dashboard = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -68,7 +69,9 @@ const Dashboard = () => {
       </div>
       <div className=" flex justify-between">
         <h1 className="text-4xl font-extrabold">My Links</h1>
-        <Button>Create Links</Button>
+        <Button>
+          <CreateLink />
+        </Button>
       </div>
       <div className="relative">
         <Input
