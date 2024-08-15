c8775333b1542885c24ccc4a917dfc2a07825a03
Ennypitan Adekoya <ennypitan@gmail.com>
2024-08-15 17:26:44 +0100

    updated

--- src/pages/Link.jsx
+++ src/pages/Link.jsx
@@ -5,12 +5,29 @@ import useFetch from "@/hooks/use-fetch";
 import { LinkIcon } from "lucide-react";
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { BarLoader } from "react-spinners";
+import { BarLoader, BeatLoader } from "react-spinners";
+import { Copy, Download, Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const Link = () => {
   const { id } = useParams();
   const { user } = UrlState();
   const navigate = useNavigate();
+
+  const downloadImage = () => {
+    const imageUrl = url?.qr;
+    const fileName = url?.title;
+
+    const anchor = document.createElement("a");
+    anchor.href = imageUrl;
+    anchor.download = fileName;
+
+    document.body.appendChild(anchor);
+
+    anchor.click();
+
+    document.body.removeChild(anchor);
+  };
 
   const {
     loading,
@@ -49,16 +66,52 @@ const Link = () => {
 
       <div className="flex flex-col gap-8 sm:flex-row justify-between">
         <div className="flex flex-col items-start gap-8 rounded-lg sm:w-2/5">
-          <span>{url?.title}</span>
-          <a href={`https://tiyp.netlify.app/${link}`} target="_blank">
+          <span className="text-6xl font-extrabold hover:underline cursor-pointer">
+            {url?.title}
+          </span>
+          <a
+            href={`https://tiyp.netlify.app/${link}`}
+            target="_blank"
+            className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
+          >
             https://tiyp.netlify.app/${link}
           </a>
-          <a href={url?.original_url} target="_blank">
+          <a
+            href={url?.original_url}
+            target="_blank"
+            className="flex items-center gap-1 hover:underline cursor-pointer"
+          >
             <LinkIcon className="p-1" /> {url?.original_url}
           </a>
-          <span>{new Date(url?.created_at).toLocaleString()}</span>
+          <span className="flex items-end font-extralight text-sm">
+            {new Date(url?.created_at).toLocaleString()}
+          </span>
+
+          <div className="flex gap-2">
+            <Button
+              variant="ghost"
+              onClick={() =>
+                navigator.clipboard.writeText(
+                  `https://tiyp.netlify.app/${url?.short_url}`
+                )
+              }
+            >
+              <Copy />
+            </Button>
+            <Button variant="ghost" onClick={downloadImage}>
+              <Download />
+            </Button>
+            <Button variant="ghost" onClick={() => fnDelete()}>
+              {loadingDelete ? <BeatLoader size={5} color="red" /> : <Trash />}
+            </Button>
+          </div>
+          <img
+            src={url?.qr}
+            className="w-full self-center sm:self-start ring ring-blue-500 p-1 object-contain"
+            alt="qr code"
+          />
         </div>
-        <div></div>
+        <div className="sm:w-3/5"></div>
       </div>
     </>
   );
